fec16df5afbd798e9ee94896cff3157cca3d812c
Raj8998 <rajpastagiya54@gmail.com>
2020-02-26 21:17:26 +0530

    applied master layout to index page. closes #18

--- pet-clinic-web/src/main/java/springframework/sfgpetclinic/controllers/IndexPageController.java
+++ pet-clinic-web/src/main/java/springframework/sfgpetclinic/controllers/IndexPageController.java
@@ -1,13 +1,15 @@
 package springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexPageController {
     @RequestMapping({"","/","index","index.html"})
-    public String getIndex()
+    public String getIndex(Model model)
     {
+        model.addAttribute("welcome","Welcome to Pet Clinic Web-App");
         return "index";
     }
 }
