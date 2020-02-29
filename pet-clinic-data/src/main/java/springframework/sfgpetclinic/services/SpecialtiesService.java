61664addf04c4be44da1afd7334835a45093cd0b
Raj8998 <rajpastagiya54@gmail.com>
2020-02-29 17:32:53 +0530

    added specialties service map. closes #30

---
+++ pet-clinic-data/src/main/java/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package springframework.sfgpetclinic.services;
+
+import springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty,Long> {
+}
