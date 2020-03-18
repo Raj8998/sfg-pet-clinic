adcea054ba7350de1519f9c45b5d04c82a097d9e
Raj8998 <rajpastagiya54@gmail.com>
2020-03-18 17:56:46 +0530

    created owner service as spring data JPA form. closes #39

--- pet-clinic-data/src/main/java/springframework/sfgpetclinic/repositories/VisitRepository.java
+++ pet-clinic-data/src/main/java/springframework/sfgpetclinic/repositories/VisitRepository.java
@@ -3,5 +3,5 @@ package springframework.sfgpetclinic.repositories;
 import org.springframework.data.repository.CrudRepository;
 import springframework.sfgpetclinic.model.Visits;
 
-public interface VisitRepositroy extends CrudRepository<Visits, Long> {
+public interface VisitRepository extends CrudRepository<Visits, Long> {
 }
